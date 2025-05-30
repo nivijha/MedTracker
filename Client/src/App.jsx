5d7d8d9041daaa0ccfdcf15f9297e0e993d72cdf
Masralai <devdeeppaul2004@gmail.com>
2025-05-30 21:51:47 +0530

    tailwind fixed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { Activity, FileText, FlaskRound as Flask, Heart, LayoutDashboard, Menu, PlusCircle, User, X , Calendar , Stethoscope, Import, Link,} from 'lucide-react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Report from '../pages/Report';
-import Settings from '../pages/Settings';
-import Profile from '../pages/Profile';
-import Navbar from '../pages/Navbar';
+import Report from '../components/Report';
+import Settings from '../components/Settings';
+import Profile from '../components/Profile';
+import Navbar from '../components/Navbar';
 
-import { ColorModeButton } from "./components/ui/color-mode"
 
 
 const DashboardCard = ({ title, value, subtitle, icon: Icon }) => (
@@ -111,7 +110,7 @@ function App() {
 
             </button>
             <div className="flex items-center space-x-4">
-              <ColorModeButton/>
+              
               <button className="flex items-center space-x-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
                 <PlusCircle className="h-5 w-5" />
                 <span>Add Report</span>
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from "./components/ui/provider"
+
 import App from './App.jsx';
 import './index.css';
 import {BrowserRouter as Router, Link, Routes, Route, BrowserRouter } from "react-router-dom";
@@ -8,9 +8,9 @@ import {BrowserRouter as Router, Link, Routes, Route, BrowserRouter } from "reac
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Provider>
+    
     <App />
-    </Provider>
+    
     </BrowserRouter>
   </StrictMode>
 );
